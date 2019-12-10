85b5ee06e01136dc8913b01cfd330d5ac5458730
Maksim Volkau <volkau.maksim@gmail.com>
2019-12-10 16:18:49 +0200

    update

--- nasos/assets/js/main.js
+++ nasos/assets/js/main.js
@@ -1,27 +1,27 @@
 
 
-// onblur 
-// InputEvent
+// // onblur 
+// // InputEvent
 
 
-function logKey(e) {
-    document.getElementById('log').innerHTML = "";
-    log.textContent += ` ${e.code}`;
-  }
-document.addEventListener('keydown', logKey);
-document.getElementsByClassName('btn')[0].addEventListener('mousedown', logKey);
-// document.getElementsByClassName('btn').onclick = logKey(this);
+// function logKey(e) {
+//     document.getElementById('log').innerHTML = "";
+//     log.textContent += ` ${e.code}`;
+//   }
+// document.addEventListener('keydown', logKey);
+// document.getElementsByClassName('btn')[0].addEventListener('mousedown', logKey);
+// // document.getElementsByClassName('btn').onclick = logKey(this);
 
-let term = "dfghdföÅ";
+// let term = "dfghdföÅ";
 
 
-function comparing(params) {
-        let re = new RegExp("^[a-zA-ZäöåÄÖÅ]+$");
-    if (re.test(term)) {
-        console.log("Valid");
-    } else {
-        console.log("Invalid");
-    }
-}
+// function comparing(params) {
+//         let re = new RegExp("^[a-zA-ZäöåÄÖÅ]+$");
+//     if (re.test(term)) {
+//         console.log("Valid");
+//     } else {
+//         console.log("Invalid");
+//     }
+// }
 
 
